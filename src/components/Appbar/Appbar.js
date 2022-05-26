d17defb7d5590332a9fdd7bd8937517174f3e0be
Selim <selim@raisissoftware.com>
2022-05-26 09:57:22 +0300

    Weather component

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,52 +1,51 @@
-import React, { useContext, useState } from "react";
-import styles from "./Appbar.module.scss";
+import React, { useContext, useState } from 'react'
+import styles from './Appbar.module.scss'
 
 // component
-import Drawer from "../Drawer/Drawer";
+import Drawer from '../Drawer/Drawer'
 
 // icons
-import { RiMenu2Fill } from "react-icons/ri";
-import { FiSearch } from "react-icons/fi";
-import { CgProfile } from "react-icons/cg";
-import { BsBookmarkDash } from "react-icons/bs";
-import { AiOutlinePlus } from "react-icons/ai";
+import { RiMenu2Fill } from 'react-icons/ri'
+import { FiSearch } from 'react-icons/fi'
+import { CgProfile } from 'react-icons/cg'
+import { BsBookmarkDash } from 'react-icons/bs'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 function Appbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-  console.log(isOpen);
-  return (
-    <nav className={styles.container}>
-      <div className={styles.container__hamburger}>
-        <RiMenu2Fill onClick={toggleMenu} />
-      </div>
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+    return (
+        <nav className={styles.container}>
+            <div className={styles.container__hamburger}>
+                <RiMenu2Fill onClick={toggleMenu} />
+            </div>
 
-      <div className={styles.container__widgets}>widgets</div>
+            <div className={styles.container__widgets}>widgets</div>
 
-      <div className={styles.container__logo}>
-        <p>N</p>
-      </div>
-      <div className={styles.container__actions}>
-        <ul>
-          <li>
-            <AiOutlinePlus />
-          </li>
-          <li>
-            <FiSearch />
-          </li>
-          <li>
-            <CgProfile />
-          </li>
-          <li>
-            <BsBookmarkDash />
-          </li>
-        </ul>
-      </div>
-      {isOpen && <Drawer />}
-    </nav>
-  );
+            <div className={styles.container__logo}>
+                <p>N</p>
+            </div>
+            <div className={styles.container__actions}>
+                <ul>
+                    <li>
+                        <AiOutlinePlus />
+                    </li>
+                    <li>
+                        <FiSearch />
+                    </li>
+                    <li>
+                        <CgProfile />
+                    </li>
+                    <li>
+                        <BsBookmarkDash />
+                    </li>
+                </ul>
+            </div>
+            {isOpen && <Drawer />}
+        </nav>
+    )
 }
 
-export default Appbar;
+export default Appbar
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,63 +1,96 @@
-import React, { useEffect } from "react";
-import styles from "./Weather.module.scss";
-import axios from "axios";
+import React, { useEffect } from 'react'
+import styles from './Weather.module.scss'
+import axios from 'axios'
+import moment from 'moment'
+import dayjs from 'dayjs'
 
-import { RiSunCloudyLine } from "react-icons/ri";
+import { RiSunCloudyLine } from 'react-icons/ri'
 
 function Weather() {
-  const [weather, setWeather] = React.useState({});
+    const [weather, setWeather] = React.useState({})
 
-  const getWeather = async () => {
-    try {
-      const res = await axios.get(
-        "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m&current_weather=true"
-      );
-      setWeather(res.data);
-    } catch (error) {
-      console.error(error);
+    // api
+    const getWeather = async () => {
+        try {
+            const res = await axios.get(
+                'https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m&current_weather=true'
+            )
+            setWeather(res.data)
+        } catch (error) {
+            console.error(error)
+        }
     }
-  };
-  useEffect(() => {
-    getWeather();
-  }, []);
+    useEffect(() => {
+        getWeather()
+    }, [])
 
-  console.log(
-    weather?.current_weather?.temperature
-      ? weather?.current_weather?.temperature
-      : "No data"
-  );
+    const timeArray = []
+    if (weather?.hourly?.time) {
+        for (let i = 1; i <= 24; i++) {
+            if (i % 3 === 0) {
+                timeArray.push(weather?.hourly?.time[i])
+            }
+        }
+    }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.container__details}>
-        <div className={styles.container__details__city}>
-          <span>
-            <RiSunCloudyLine />
-          </span>
-          <h2>Mostly Cloudy</h2>
-          <h3>New York</h3>
-        </div>
-        <div className={styles.container__details__degree}>
-          <ul>
-            <li>Today</li>
-            <li>Tomorrow</li>
-            <li>Week</li>
-          </ul>
-          <h1>{}</h1>
-        </div>
-      </div>
-      <div>
-        {/* <ul>
-          {weather.hourly.temperature_2m.map((item) => (
-            <li key={item}>
-              <h6>65°</h6>
-              <p>6 AM</p>
-            </li>
-          ))}
-        </ul> */}
-      </div>
-    </div>
-  );
+    const tempArray = []
+    if (weather?.hourly?.temperature_2m) {
+        for (let i = 1; i <= 24; i++) {
+            if (i % 3 === 0) {
+                tempArray.push(weather?.hourly?.temperature_2m[i])
+            }
+        }
+    }
+
+    const finalArray = []
+    for (let i = 0; i < timeArray.length; i++) {
+        finalArray.push({
+            time: timeArray[i],
+            temp: tempArray[i],
+        })
+    }
+
+    return (
+        <>
+            {weather?.current_weather?.temperature ? (
+                <div className={styles.container}>
+                    <div className={styles.container__details}>
+                        <div className={styles.container__details__city}>
+                            <span>
+                                <RiSunCloudyLine />
+                            </span>
+                            <h2>Mostly Cloudy</h2>
+                            <h3>New York</h3>
+                        </div>
+                        <div className={styles.container__details__degree}>
+                            <ul>
+                                <li>Today</li>
+                                <li>Tomorrow</li>
+                                <li>Week</li>
+                            </ul>
+                            <h1>
+                                {weather?.current_weather?.temperature &&
+                                    weather?.current_weather?.temperature + '°'}
+                            </h1>
+                        </div>
+                    </div>
+                    <div className={styles.container__hourly}>
+                        <ul>
+                            {finalArray.map((item, index) => (
+                                <li key={index}>
+                                    <h6>{item.temp}</h6>
+                                    <p>{moment(item.time).format('h a')}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.nodata}>
+                    <h1>Loading data...</h1>
+                </div>
+            )}
+        </>
+    )
 }
-
-export default Weather;
+export default Weather
